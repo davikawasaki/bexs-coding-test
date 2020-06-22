b3cae2a7e79720591a18e584a62fb36cfc5a958f
Davi Kawasaki <davishinjik@gmail.com>
2020-06-22 16:59:22 -0300

    Improving and fixing string package tests

--- services/utils/string_test.go
+++ services/utils/string_test.go
@@ -35,20 +35,44 @@ func TestCompareStringArrays(t *testing.T) {
 	outputWrong2 := []string{}
 
 	if !CompareStringArrays(outputWrong1, data) {
-		t.Errorf("CompareStringArrays() with different arrays PASSED, expected %v but got %v", data, outputWrong1)
+		t.Logf("CompareStringArrays() with different arrays PASSED, expected %v and got %v", data, outputWrong1)
+	} else {
+		t.Errorf("CompareStringArrays() with different arrays FAILED, expected %v but got %v", data, outputWrong1)
 	}
 
 	if !CompareStringArrays(outputWrong2, data) {
-		t.Errorf("CompareStringArrays() with empty string array PASSED, expected %v but got %v", data, outputWrong2)
+		t.Logf("CompareStringArrays() with empty string array PASSED, expected %v and got %v", data, outputWrong2)
+	} else {
+		t.Errorf("CompareStringArrays() with empty string array FAILED, expected %v but got %v", data, outputWrong2)
 	}
 
 	if !CompareStringArrays(data, outputWrong2) {
-		t.Errorf("CompareStringArrays() with empty string array PASSED, expected %v but got %v", outputWrong2, data)
+		t.Logf("CompareStringArrays() with empty string array PASSED, expected %v and got %v", outputWrong2, data)
+	} else {
+		t.Errorf("CompareStringArrays() with empty string array FAILED, expected %v but got %v", outputWrong2, data)
 	}
 
 	if CompareStringArrays(outputCorrect, data) {
 		t.Logf("CompareStringArrays() PASSED, expected %v and got %v", data, outputCorrect)
 	} else {
 		t.Errorf("CompareStringArrays() should have passed but FAILED, expected %v but got %v", data, outputCorrect)
+	}
+}
+
+func TestTrimAndUpper(t *testing.T) {
+	dataItems := []TestDataItem{
+		{"BRC ", "BRC", true},
+		{" ORL", "ORL", true},
+		{" cdg ", "CDG", true},
+	}
+
+	for _, item := range dataItems {
+		result := TrimAndUpper(item.input)
+
+		if result != item.output {
+			t.Errorf("TestTrimAndUpper() with args %v FAILED, expected '%v' but got value '%v'", item.input, item.output, result)
+		} else {
+			t.Logf("TestTrimAndUpper() with args %v PASSED, expected '%v' and got value '%v'", item.input, item.output, result)
+		}
 	}
 }
